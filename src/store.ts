f57a1d8f88a4415c7554ffa0d7ba61cb3e448ec2
Tetsuya Mori <monry84@gmail.com>
2021-09-27 01:08:27 +0900

    :recycle: Remove unnecessary slice

--- src/store.ts
+++ src/store.ts
@@ -2,13 +2,11 @@ import {configureStore} from "@reduxjs/toolkit";
 import {stepsSlice} from "./slices/steps";
 import {gameSlice} from "./slices/game";
 import {useSelector, TypedUseSelectorHook, useDispatch} from "react-redux";
-import {cellSlice} from "./slices/cell";
 
 export const store = configureStore({
     reducer: {
         steps: stepsSlice.reducer,
         game: gameSlice.reducer,
-        cell: cellSlice.reducer,
     },
 });
 
